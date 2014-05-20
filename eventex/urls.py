38417bc47bf2293f3b68cb2e0e8e95bfbc984425
helton <helton99@hotmail.com>
2014-05-20 13:23:18 +0100

    Started projetc wttd - Eventex.

---
+++ eventex/urls.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+from django.conf.urls import patterns, include, url
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
+from django.contrib import admin
+admin.autodiscover()
+
+from core.views import Home
+
+urlpatterns = patterns('',
+    url(r'^$', Home.as_view(), name='home'),
+
+    url(r'^admin/', include(admin.site.urls)),
+
+)
+
+urlpatterns += staticfiles_urlpatterns()
